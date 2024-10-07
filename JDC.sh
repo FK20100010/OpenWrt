8a7127245d2f309c9376f5137bf5c2d3f5802207
FK20100010 <154255559+FK20100010@users.noreply.github.com>
2024-10-07 14:05:36 +0800

    Update JDC.sh

--- JDC.sh
+++ JDC.sh
@@ -1,60 +1,49 @@
-#!/bin/bash
-#=================================================
-#   Description: DIY script
-#   Lisence: MIT
-#   Author: P3TERX
-#   Blog: https://p3terx.com
-#=================================================
-#克隆源码
-git clone -b ipq60xx-devel_nss --single-branch https://github.com/JiaY-shi/openwrt openwrt
+git clone -b main --single-branch https://github.com/LiBwrt-op/openwrt-6.x.git openwrt
 [ -e files ] && mv files openwrt/files
 cd openwrt
-#添加passwall
+
+#sed -i '$a src-git other https://github.com/Lienol/openwrt-package.git;other' feeds.conf.default
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+
 #sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
 #sed -i '$a src-git xiaorouji1 https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
-./scripts/feeds clean
+#sed -i '$a src-git-full x https://github.com/x-wrt/com.x-wrt.git' feeds.conf.default
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-cp /usr/bin/upx staging_dir/host/bin
-cp /usr/bin/upx-ucl staging_dir/host/bin
-
 #添加自定义插件
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant package/luci-app-fileassistan
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cpufreq package/luci-app-cpufreq
-#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-netdata package/luci-app-netdata
-git clone https://github.com/ssuperh/luci-app-vlmcsd-new.git package/luci-app-vlmcsd-new
-git clone https://github.com/flytosky-f/openwrt-vlmcsd.git package/openwrt-vlmcsd
-git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-2.3.1
-#svn checkout https://github.com/Hyy2001X/AutoBuild-Packages/trunk/luci-app-webd package/luci-app-webd
-#svn checkout https://github.com/Hyy2001X/AutoBuild-Packages/trunk/webd package/webd
-#git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
-git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
-git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
-#git clone https://github.com/dazhaolear/luci-app-autorebootnew.git package/luci-app-autorebootnew
-#修改Samba4d的位置
-#sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/lib/lua/luci/controller/samba4.lua" package/default-settings/files/zzz-default-settings
-#sed -i "/exit 0/i\sed -i 's/services/nas/g' /usr/share/luci/menu.d/luci-app-samba4.json" package/default-settings/files/zzz-default-settings
-#sed -i 's/services/nas/g' feeds/luci/applications/luci-app-ksmbd/root/usr/share/luci/menu.d/luci-app-ksmbd.json
-#sed -i 's/services/nas/g' feeds/luci/applications/luci-app-samba4/root/usr/share/luci/menu.d/luci-app-samba4.json
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b js --single-branch https://github.com/papagaye744/luci-theme-design.git package/luci-theme-design
 
+rm -rf feeds/packages/net/chinadns-ng
+cp -rf feeds/passwall_packages/chinadns-ng/ feeds/packages/net/
+rm -rf feeds/luci/applications/luci-app-passwall/
+cp -rf feeds/passwall/luci-app-passwall/ feeds/luci/applications
+
+# TTYD 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+#rm -rf package/libs/ustream-ssl
+#rm -rf feeds/packages/net/miniupnpd
+#svn co https://github.com/x-wrt/x-wrt/trunk/package/libs/ustream-ssl package/ustream-ssl
+#svn co https://github.com/x-wrt/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
+         
 #修改lan口地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 #修改机器名称
-sed -i 's/OpenWrt/JDC/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/JDCloud/g' package/base-files/files/bin/config_generate
 
-#修改wifi名称
-sed -i 's/OpenWrt/JDC-WiFi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 #默认打开WiFi
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/disabled=1/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.sh
+sed -i 's/disabled=1/disabled=0/g' package/network/config/wifi-scripts/files/lib/wifi/mac80211.uc
 
 #修改时区
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-
-
 mv -f ../Google-Wifi/jdc.config .config
+
+#make V=s -j$(nproc)
